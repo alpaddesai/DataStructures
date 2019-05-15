1e7f060aa86bc20f08c2b00e5d17a87e57a50ff0
Alpa D. Desai <50643387+alpaddesai@users.noreply.github.com>
2019-05-14 21:38:41 -0400

    Update TestCaseMain.cpp

--- TestCaseMain.cpp
+++ TestCaseMain.cpp
@@ -35,4 +35,31 @@ return 0;
 
 }
 
+/*
+ Super Market Customer Line Consists of 
+ Cost of Groceries For Customer Person Urgent is $85
+ Cost of Groceries For Customer Alpa is $120
+ Cost of Groceries For Customer Joe is $150
+ Cost of Groceries For Customer Heather is $80
+ Cost of Groceries For Customer Kim is $60
+ Cost of Groceries For Customer Alan is $90
 
+ Name of the customer at the register is Person Urgent
+ Person  Person Urgent is a rewards member hence cost of groceries with 20% discount is $76.5
+
+ Name of the customer at the register is Alpa
+ Person  Alpa is a rewards member hence cost of groceries with 20% discount is $108
+
+ Name of the customer at the register is Joe
+ Person  Joe is a rewards member hence cost of groceries with 20% discount is $135
+
+ Name of the customer at the register is Heather
+ Person  Heather is a not a rewards member hence no discount and cost of groceries is $80
+
+ Name of the customer at the register is Kim
+ Person  Kim is a rewards member hence cost of groceries with 20% discount is $54
+
+ Name of the customer at the register is Alan
+ Person  Alan is a not a rewards member hence no discount and cost of groceries is $90
+
+*/
